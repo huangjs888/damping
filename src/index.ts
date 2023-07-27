532a686aae6dceeb7009f9505d4434fb847b957f
huangjs888 <1170384651@qq.com>
2023-07-27 14:34:56 +0800

    更改接口

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,16 @@
  * @Author: Huangjs
  * @Date: 2023-02-13 15:22:58
  * @LastEditors: Huangjs
- * @LastEditTime: 2023-07-21 16:31:01
+ * @LastEditTime: 2023-07-27 14:34:11
  * @Description: ******
  */
 
-const expo = 0.88;
-
-function damping(value: number, max: number, revoke: boolean = false) {
+function damping(
+  value: number,
+  max: number,
+  expo: number,
+  revoke: boolean = false,
+) {
   if (value < 1) {
     return value;
   }
@@ -25,41 +28,48 @@ function damping(value: number, max: number, revoke: boolean = false) {
   return max / (1 + (max - 1) / Math.pow(value, expo));
 }
 
+type DampingOption = {
+  max?: number;
+  mode?: number;
+  expo?: number;
+};
 // 阻尼值
-export function performDamping(value: number, max: number, mode: number = 0) {
+export function performDamping(value: number, option: DampingOption = {}) {
   if (value === 0) {
     return 0;
   }
+  const { max = 9999, mode = 0, expo = 0.88 } = option;
   let _value = Math.abs(value);
   if (_value < 1) {
     if (mode === 1) {
       // 倒数模式
-      _value = 1 / damping(1 / _value, max);
+      _value = 1 / damping(1 / _value, max, expo);
     } else {
       // 加1模式
-      _value = damping(_value + 1, max) - 1;
+      _value = damping(_value + 1, max, expo) - 1;
     }
   } else {
-    _value = damping(_value, max);
+    _value = damping(_value, max, expo);
   }
   return _value * (value > 0 ? 1 : -1);
 }
 // 阻尼原值
-export function revokeDamping(value: number, max: number, mode: number = 0) {
+export function revokeDamping(value: number, option: DampingOption = {}) {
   if (value === 0) {
     return 0;
   }
+  const { max = 9999, mode = 0, expo = 0.88 } = option;
   let _value = Math.abs(value);
   if (_value < 1) {
     if (mode === 1) {
       // 倒数模式
-      _value = 1 / damping(1 / _value, max, true);
+      _value = 1 / damping(1 / _value, max, expo, true);
     } else {
       // 加1模式
-      _value = damping(_value + 1, max, true) - 1;
+      _value = damping(_value + 1, max, expo, true) - 1;
     }
   } else {
-    _value = damping(_value, max, true);
+    _value = damping(_value, max, expo, true);
   }
   return _value * (value > 0 ? 1 : -1);
 }
